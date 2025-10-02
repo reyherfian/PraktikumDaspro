22939df3ca6b38805f99f60ff778fb544a9fc014
reyherfian <rey.adriano.m@gmail.com>
2025-10-02 14:05:37 +0700

    modifikasi percobaan 1

--- jobsheet5/ifCetakKRS03.java
+++ jobsheet5/ifCetakKRS03.java
@@ -14,5 +14,9 @@ public class ifCetakKRS03 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan PDA");
         }
+
+        else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
 }
