85e56dfea5be8905475e0a07427f943b91a8f9cd
reyherfian <rey.adriano.m@gmail.com>
2025-10-03 10:02:21 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS03.java
+++ jobsheet5/ifCetakKRS03.java
@@ -10,13 +10,15 @@ public class ifCetakKRS03 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan PDA");
-        }
+        String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi \nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+        System.out.println(pesan);
+        //     if (uktLunas) {
+    //         System.out.println("Pembayaran UKT terverifikasi");
+    //         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    //     }
 
-        else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+    //     else {
+    //         System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+    //     }
     }
 }
