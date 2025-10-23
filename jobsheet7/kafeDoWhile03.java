24f1682288aa200af90f80ab0f4985f5c077579b
reyherfian <rey.adriano.m@gmail.com>
2025-10-23 12:37:12 +0700

    Percobaan 3

---
+++ jobsheet7/kafeDoWhile03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class kafeDoWhile03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int kopi, teh, roti, totalHarga;
+        int hKopi = 12000, hTeh = 7000, hRoti = 20000;
+        String namaPelanggan;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik batal untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+
+            System.out.print("Jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi * hKopi) + (teh * hTeh) + (roti * hRoti);
+            System.out.println("Total yang harus dibayar: Rp " + totalHarga);
+            sc.nextLine(); 
+
+        } while (true);
+
+        System.out.println("Semua transaksi selesai.");
+    }
+}
