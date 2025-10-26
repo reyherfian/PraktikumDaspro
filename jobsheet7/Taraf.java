586c2fc52ef5ce50556eb3aaac0d4699b521be8c
reyherfian <rey.adriano.m@gmail.com>
2025-10-26 21:43:11 +0700

    Tugas 2

---
+++ jobsheet7/Taraf.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Taraf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jenis, durasi;
+        double total = 0;
+
+        do {
+            System.out.print("Masukkan jenis kendaraan (1 = mobil, 2 = motor, 0 = keluar: ");
+            jenis = sc.nextInt();
+
+            if (jenis == 0) {
+                break;
+            }
+            if (jenis == 1 || jenis == 2) {
+                System.out.print("Masukkan durasi parkir (jam): ");
+                durasi = sc.nextInt();
+
+                if (durasi > 5) {
+                    total += 12500;
+                } else if (jenis == 1) {
+                    total += durasi * 3000;
+                } else {
+                    total += durasi * 2000;
+                }
+            } else {
+                System.out.print("Input tidak valid, coba lagi!");
+            }
+        } while (jenis != 0);
+        System.out.print("Total pendapatan parkir hari ini: Rp. " + total);
+    }
+}
