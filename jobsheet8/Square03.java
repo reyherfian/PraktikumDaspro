efbb924af917d4977f6629f79ad518eb2c539194
reyherfian <rey.adriano.m@gmail.com>
2025-11-02 21:45:21 +0700

    Modifikasi percobaan 2

--- jobsheet8/Square03.java
+++ jobsheet8/Square03.java
@@ -6,10 +6,9 @@ public class Square03 {
         System.out.print("Masukkan nilai N = ");
         int N = sc.nextInt();
         for (int iOuter = 1; iOuter <= N; iOuter++) {
-            for (int i = 1; i <= N; i++) {
+            for (int i = 0; i <= N; i++) {
                 System.out.print("*");
             }
-            System.out.println();
         }
     }
 }
