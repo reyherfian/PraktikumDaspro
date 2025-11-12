3f056e6856165211f2c401bbde4abe877acc96d6
reyherfian <rey.adriano.m@gmail.com>
2025-11-12 18:22:13 +0700

    Modifikasi percobaan 1

---
+++ jobsheet9/ArrayBilangan03.java
@@ -0,0 +1,9 @@
+public class ArrayBilangan03 {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -17, 17};
+
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}
