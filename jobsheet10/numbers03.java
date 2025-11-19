11986285efebef548bda7cc4bbd4b189dc4ab424
reyherfian <rey.adriano.m@gmail.com>
2025-11-19 18:34:53 +0700

    Percobaan 3

---
+++ jobsheet10/numbers03.java
@@ -0,0 +1,9 @@
+public class numbers03 {
+    public static void main(String[] args) {
+        
+        int myNum[][] = new int [3][];
+        myNum[0] = new int[5];
+        myNum[1] = new int[3];
+        myNum[2] = new int[1];
+    }
+}
