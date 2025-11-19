cbbfd3b271568f5e9618ab47fe0b08f3eda4b07c
reyherfian <rey.adriano.m@gmail.com>
2025-11-19 18:52:13 +0700

    Modifikasi percobaan 3

--- jobsheet10/numbers03.java
+++ jobsheet10/numbers03.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class numbers03 {
     public static void main(String[] args) {
         
@@ -5,5 +7,12 @@ public class numbers03 {
         myNum[0] = new int[5];
         myNum[1] = new int[3];
         myNum[2] = new int[1];
+        
+        for (int i = 0; i < myNum.length; i++) {
+            System.out.println(Arrays.toString(myNum[i]));
+        }
+        for (int i = 0; i < myNum.length; i++) {
+            System.out.println("Panjang baris ke-" +(i+1) + ": " + myNum[i].length);
+        }
     }
 }
