d0f27737e406629770093bd3c2593e0674ce2a70
reyherfian <rey.adriano.m@gmail.com>
2025-12-01 13:37:31 +0700

    percobaan 2

--- jobsheet11/kafe03.java
+++ jobsheet11/kafe03.java
@@ -1,5 +1,11 @@
 public class kafe03 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMmember) {
+        System.out.println("Selamat datang " + namaPelanggan + "!");
+
+        if (isMmember) {
+            System.out.println("Anda adalah member! dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam Rp 15,000");
         System.out.println("2. Cappuccino Rp 20,000");
@@ -12,7 +18,7 @@ public class kafe03 {
     }
 
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 }
 
