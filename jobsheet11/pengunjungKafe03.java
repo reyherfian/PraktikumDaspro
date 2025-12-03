a54f1db4477a37e3232d46eacf2e5a7dd8103137
reyherfian <rey.adriano.m@gmail.com>
2025-12-03 16:38:29 +0700

    percobaan 4

---
+++ jobsheet11/pengunjungKafe03.java
@@ -0,0 +1,14 @@
+public class pengunjungKafe03 {
+    static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar nama pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+            
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
