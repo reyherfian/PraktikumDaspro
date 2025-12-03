8ba2bb0cf65c9a07b09623cf65b63fa4fe88c039
reyherfian <rey.adriano.m@gmail.com>
2025-12-03 15:54:24 +0700

    percobaan 3

--- jobsheet11/kafe03.java
+++ jobsheet11/kafe03.java
@@ -1,3 +1,4 @@
+import java.util.Scanner; 
 public class kafe03 {
     public static void Menu(String namaPelanggan, boolean isMmember, String kodePromo) {
         System.out.println("Selamat datang " + namaPelanggan + "!");
@@ -13,7 +14,9 @@ public class kafe03 {
         } else {
             System.out.println("kode promo invalid!");
         }
+    }
 
+    public static void Menu() {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam Rp 15,000");
         System.out.println("2. Cappuccino Rp 20,000");
@@ -25,8 +28,27 @@ public class kafe03 {
         System.out.println("Silakan pilih menu yang Anda inginkan.");
     }
 
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakMenu) {
+        int [] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakMenu;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
-        Menu("Budi", true, "DISKON30");
+        Scanner sc = new Scanner(System.in);
+
+        Menu ();
+        
+        System.out.print("\nMasukkan nomer menu yang ingin anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakMenu = sc.nextInt();
+
+        int hargaTotal = hitungTotalHarga(pilihanMenu, banyakMenu);
+
+        System.out.print("Total harga untuk pesanan anda: Rp. " + hargaTotal);
     }
 }
 
